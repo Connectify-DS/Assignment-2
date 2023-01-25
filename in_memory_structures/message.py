0dd79fa7a3a26d9ad1b19ef552324abc7df02bc1
YASHICA PATODIA <58307462+yashica-patodia@users.noreply.github.com>
2023-01-25 18:02:51 +0530

    Adding in-memory structures

---
+++ in_memory_structures/message.py
@@ -0,0 +1,23 @@
+class MessageTable:
+    def __init__(self):
+        self.message_table_entries = {}
+        self.last_id = 0
+
+    def get_new_id_auto_inc(self):
+        self.last_id += 1
+        return self.last_id
+
+    def get_message(self, message_id):
+        if message_id in self.message_table_entries:
+            return self.message_table_entries[message_id]
+
+    def add_message(self, message):
+        message_id = self.get_new_id_auto_inc()
+        if message_id not in self.message_table_entries:
+            self.message_table_entries[message_id] = Message(message)
+        return message_id
+
+
+class Message:
+    def __init__(self, message):
+        self.message = message
---
+++ in_memory_structures/topics.py
@@ -0,0 +1,45 @@
+class TopicTable:
+    def __init__(self):
+        self.topic_queues = {}
+        self.last_id = 0
+
+    def get_topic_queue(self, topic_name):
+        return self.topic_queues[topic_name]
+
+    def get_topic_list(self):
+        return list(self.topic_queues.keys())
+
+    def create_topic_queue(self, topic_name):
+        self.topic_queues[topic_name] = TopicQueue(topic_name)
+
+
+class TopicQueue:
+    def __init__(self, topic_name):
+        self.topic_name = topic_name
+        self.topic_queue = []
+
+    def get_reader_lock(self):
+        pass
+
+    def get_writer_lock(self):
+        pass
+
+    def release_reader_lock(self):
+        pass
+
+    def release_writer_lock(self):
+        pass
+
+    def enqueue(self, message):
+        self.topic_queue.append(message)
+
+    def get_at_offset(self, offset):
+        if offset >= len(self.topic_queue):
+            return None
+        return self.topic_queue[offset]
+
+    def size(self):
+        return len(self.topic_queue)
+
+    def can_write(self):
+        return True
