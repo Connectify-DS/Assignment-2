2d704e62bf9547ca71c47c9f13264cf17fd0c1c0
Riyuk-04 <ishangoel04062001@gmail.com>
2023-02-27 18:40:21 +0530

    write-manager-functions

--- models/write_manager.py
+++ models/write_manager.py
@@ -3,6 +3,7 @@ from config import *
 from broker import broker
 from in_memory_structures import ConsumerTable, ProducerTable, TopicTable, MessageTable
 from database_structures import ConsumerDBMS, ProducerDBMS, TopicDBMS, MessageDBMS
+import random
 # TODO:
 # Move list_topics register producer and register consumer 
 # Write a MyWriteManager class that sends requests to flask server
@@ -11,11 +12,15 @@ from database_structures import ConsumerDBMS, ProducerDBMS, TopicDBMS, MessageDB
 # Use MyBroker class from myqueue folder to create, publish, consume, list topics as that class
 # requests the broker server. Initialise it with the broker url. 
 class writeManager:
-    def __init__(self, config,init_brokers = 1):
-        self.partition_broker = {}
-        self.topic_numPartitions = {}
-        self.broker_port = {}
-        self.brokers=[] ## List of instances of MyBroker Class
+    def __init__(self, config, init_brokers = 1):
+        self.topics = []
+        self.partition_broker = {}      #Partition -> Broker
+        self.topic_numPartitions = {}  #Topic -> num_partition
+        self.broker_port = {}  #Broker ID -> Broker Port
+        self.brokers=[] ## List of id of MyBroker Class
+        self.producer_topic = {}
+
+        self.num_producers = 0
         self.num_brokers = init_brokers
         self.ispersistent = config['IS_PERSISTENT']
 
@@ -39,33 +44,56 @@ class writeManager:
             # self.topic_table = TopicTable()
         
     def add_broker(self,port):
-        ## Note: You will have to request read manager to add this broker too. 
+        ## Note: You will have to request read manager to add this broker too.
         # port = insert random port
         self.num_brokers += 1
+        broker_id = self.num_brokers
+        self.brokers.append(broker_id)
         # Create new broker server here: By Running Flask App
         # Create Instance of MyBroker Class with base url (LOCALHOST:PORT) to send requests
         # new_broker = MyBroker(LOCALHOST:PORT) -> self.brokers.append(new_broker)
         self.broker_port[self.num_brokers] = port
 
     def add_topic(self, topic_name):
-        ## Need to send request to read manager too. 
+        ## Need to send request to read manager too.
         # Check if topic already exists
         # Create first partition for the topic by requesting a MyBroker instance (create_topic method)
         # Handle Metadata of Write Manager
-        pass
+        try:
+            if topic_name in self.topics:
+                raise Exception("Topic already exists")
+        except Exception as e:
+            raise e
+
+        self.topics.append(topic_name)
+        #Selecting random broker
+        curr_id = random.choice(self.brokers)
+        curr_port = self.broker_port[curr_id]
+
+        #Send topic and partition request to broker server port with following partition id
+        partition_id = topic_name + ".1"
+        self.partition_broker[partition_id] = curr_id
+        self.topic_numPartitions[topic_name] = 1
 
     def add_partition(self,topic_name):
         ## Need to send request to read manager too.
         # Choose a Broker (Round Robin / Random)
         # Create the partition by calling create_topic of MyBroker instance
-        pass
+        curr_id = random.choice(self.brokers)
+        curr_port = self.broker_port[curr_id]
+
+        self.topic_numPartitions[topic_name] += 1
+        partition_id = topic_name + "." + self.topic_numPartitions[topic_name]
+        self.partition_broker[partition_id] = curr_id
+        #Send request to broker server for creating new partition
+
 
     def list_topics(self):
         """
         Returns a list of all the topics in the system.
         """
         # Return from Metadata of Write Manager
-        pass
+        return self.topics
 
     def register_producer(self,topic_name):
         # Check if Topic Exists. If not Create Topic
@@ -76,7 +104,13 @@ class writeManager:
         #       Add new producer to the healthcheck list with the ID
         #       Save the current time (time.datetime) as the time of creation
         #       You will also have to maintain the last use time (currently empty)
-        pass
+        if topic_name not in self.topics:
+            self.add_topic(topic_name)
+
+        self.num_producers += 1
+        self.producer_topic[self.num_producers] = topic_name 
+
+        
 
     def produce_message(self,producer_id,topic_name,message):
         # Check if Producer can publish to the topic.
@@ -85,7 +119,19 @@ class writeManager:
 
         ## Health Check: 
         #       Update the last use time of the producer based on the producer id
-        pass
+        try:
+            if self.producer_topic[producer_id] != topic_name:
+                raise Exception("ProducerId is not subscribed to the topic")
+        except Exception as e:
+            raise e
+
+        curr_partition = random.randbytes(1,self.topic_numPartitions[topic_name])
+        partition_id = topic_name + "." + curr_partition
+
+        curr_broker = self.partition_broker[partition_id]
+        curr_port = self.broker_port[curr_broker]
+        #send request to broker for enque message
+
 
     def health_check(self):
         # This function will check the last use time of the producers and log whether 
